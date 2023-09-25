d90b1f9cedbe32e155f2a45cebf65a100c9f5d01
GeroPazPapa <geronimonicolaspazpapa@gmail.com>
2023-09-25 18:23:19 -0300

    imagen solucionada

--- src/portfolio.js
+++ src/portfolio.js
@@ -283,7 +283,7 @@ const bigProjects = {
       ]
     },
     {
-      image: require("./assets/images/reverberi2.jpeg"),
+      image: require("./assets/images/reverberi.jpeg"),
       projectName: "Reverberi Architect",
       projectDesc:
         "Mi historia con este proyecto o pagina web, empezo en 2022 cuando al crearle una carta de presentacion, para que pueda mostrar sus obras, a un arquitecto que vive en Manhattan, tuvimos una muy buena relacion laboral, por lo que le di la confianza para que me invitara luego a trabajar y vivir unos meses por allá!",
